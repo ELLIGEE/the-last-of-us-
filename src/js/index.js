c166f77e13027464e0e16ba4f3a214de0035ccb5
João Guilherme David <56896476+ELLIGEE@users.noreply.github.com>
2023-03-12 17:09:54 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,27 @@
+const botaoesCarrossel = document.querySelectorAll('.botao')
+const imagens = document.querySelectorAll('.imagem')
+
+botaoesCarrossel.forEach((botao, indice) => {
+
+    botao.addEventListener('click', () => {
+
+        desativarBotaoSelecionado()
+
+        botao.classList.add('selecionado')
+
+        esconderImagemAtiva()
+
+        imagens[indice].classList.add('ativa')
+    })
+})
+
+function esconderImagemAtiva() {
+    const imagemAtiva = document.querySelector('.ativa')
+    imagemAtiva.classList.remove('ativa')
+}
+
+function desativarBotaoSelecionado() {
+    const botaoSelecionado = document.querySelector('.selecionado')
+
+    botaoSelecionado.classList.remove('selecionado')
+}
